d573b3af1ffdfe4a188c8bd0d4e775b927303006
Tasir Ullah <https://github.com/Tasirullahcoder/WDD18>
2026-02-23 22:04:42 +0500

    Update Portfolio Component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,18 +5,18 @@ function Portfolio() {
   );
 
   return (
-    <section className="py-16 px-6 md:px-12 bg-gray-50">
+    <section className="  py-16 px-6 md:px-12 bg-[#fff] text-[#28282b]  flex-wrap text-[17px] leading-[27.2px] pt-[150px] px-[50px] pb-[50px]">
       <h2 className="text-[#17479e] font-DM Serif Display text-[52px] leading-[67.6px] font-bold text-center mb-8">
         We Worked With
       </h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 items-center">
+      <div className="grid grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-3 items-center">
         {clients.map((src, i) => (
           <div key={i} className="flex justify-center">
             <img
               src={src}
               alt={`Client ${i + 1}`}
-              className="w-418 h-126 object-contain"
+              className="w-418 h-27 object-contain inline-block text-[#28282b] text-[17px] leading-[27.2px] text-center "
             />
           </div>
         ))}
